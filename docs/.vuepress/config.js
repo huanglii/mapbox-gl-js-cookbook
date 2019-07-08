f1d73a325aded5bba08427b62acca5b6aae8df46
huangli <849151701@qq.com>
2019-07-08 18:07:47 +0800

    init commit

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,14 +1,19 @@
 module.exports = {
   title: 'cookbook',
-  base: 'mapbox-gl-js-cookbook',
+  base: '/mapbox-gl-js-cookbook/',
   description: 'mapbox-gl-js-cookbook',
+  locales: {
+    '/': {
+      lang: 'zh-CN', 
+    }
+  },
   head: [
     ['link', { rel: 'icon', href: '/logo.png' }]
   ],
   themeConfig: {
     nav: [
       { text: '主页', link: '/' },
-      { text: 'GISer空间', link: 'https://giser.xyz' },
+      { text: 'GISer 空间', link: 'https://giser.xyz' },
     ],
     sidebar: {
       '/data/': [
@@ -19,24 +24,10 @@ module.exports = {
         ''
       ]
     },
-    lastUpdated: 'Last Updated', // string | boolean
-    // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
+    lastUpdated: '上次更新',
     repo: 'huanglii/mapbox-gl-js-cookbook',
-    // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
-    // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
-    repoLabel: '查看源码',
-
-    // 以下为可选的编辑链接选项
-
-    // 假如你的文档仓库和项目本身不在一个仓库：
-    // docsRepo: 'vuejs/vuepress',
-    // 假如文档不是放在仓库的根目录下：
     docsDir: 'docs',
-    // 假如文档放在一个特定的分支下：
-    // docsBranch: 'master',
-    // 默认是 false, 设置为 true 来启用
     editLinks: true,
-    // 默认为 "Edit this page"
-    editLinkText: '帮助我们改善此页面！'
+    editLinkText: '在 GitHub 上编辑此页！'
   }
 }
