75dda37019de7071f6b6f8f0ae8b0a3705d2a100
huangli <849151701@qq.com>
2019-07-12 16:26:17 +0800

    add style-spec

--- docs/.vuepress/components/base-map.vue
+++ docs/.vuepress/components/base-map.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :id="container" :style="{ width: '100%', height: '400px' }"></div>
+  <div :id="container" :style="{ width: '100%', height: '400px', borderRadius: '6px' }"></div>
 </template>
 
 <script>
--- docs/.vuepress/components/code-view.vue
+++ docs/.vuepress/components/code-view.vue
@@ -16,20 +16,20 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
 .code-view-wrapper {
   position: relative;
   margin: 10px 0;
-}
-.code-view-wrapper .link {
-  position: absolute;
-  right: 10px;
-  top: 10px;
-  padding: 0 7px;
-  border-radius: 4px;
-  font-size: 14px;
-  line-height: 30px;
-  background-color: #fff;
-  box-shadow: 0 0 0 2px rgba(0, 0, 0, 0.1);
+  .link {
+    position: absolute;
+    right: 10px;
+    top: 10px;
+    padding: 0 7px;
+    border-radius: 4px;
+    font-size: 14px;
+    line-height: 30px;
+    background-color: #fff;
+    box-shadow: 0 0 0 2px rgba(0, 0, 0, 0.1);
+  }
 }
 </style>
